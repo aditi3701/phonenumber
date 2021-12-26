29ee8eb0b847eee35bd1b76165dfb362505a4662
aditi3701 <80256674+aditi3701@users.noreply.github.com>
2021-12-26 13:56:24 +0530

    Add files via upload

---
+++ phonenumber.py
@@ -0,0 +1,15 @@
+import phonenumbers
+from phonenumbers import carrier,geocoder,timezone
+
+mobileNo = input("Enter number with country code:")
+mobileNo = phonenumbers.parse(mobileNo)
+
+print(timezone.time_zones_for_number(mobileNo))
+
+print(carrier.name_for_number(mobileNo,"en"))
+
+print(geocoder.description_for_number(mobileNo,"en"))
+
+print("valid mobile no:",phonenumbers.is_valid_number(mobileNo))
+
+print("checking possibility of number:",phonenumbers.is_possible_number(mobileNo))
